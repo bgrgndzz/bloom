5e4ad389206adf3378ce855e3f2d6f692c8ac580
Buğra Gündüz <bgrgndzz@gmail.com>
2018-10-04 10:03:05 +0300

    initialize react native

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
