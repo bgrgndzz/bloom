826ae6ae8856a210da74069024296e1078b9b7d7
Buğra Gündüz <bgrgndzz@gmail.com>
2019-01-29 00:38:56 +0300

    change the sorting algorithm for popular topics

--- server/controllers/topics/list.js
+++ server/controllers/topics/list.js
@@ -8,22 +8,34 @@ module.exports = (req, res, next) => {
 
   Topic
     .find()
+    .populate('posts')
     .sort('-lastDate')
     .exec((err, topics) => {
+      if ((req.params && req.params.sort === 'popular') || (!req.params || !req.params.sort)) {
+        const rank = topic => {
+          const topicLifetime = moment().diff(moment(topic.posts[0].date), 'minutes') / (60 * 24);
+          let postCount, divisor;
+          if (topicLifetime > 1) {
+            postCount = topic.posts.filter(post => moment().diff(moment(post.date), 'minutes') / (60 * 24) <= 1).length;
+            divisor = 1;
+          } else if (topicLifetime < 0.25) {
+            postCount = topic.posts.length;
+            divisor = 0.25;
+          } else {
+            postCount = topic.posts.length;
+            divisor = topicLifetime;
+          }
+
+          return postCount / divisor;
+        };
+        topics = topics.sort((prev, cur) => rank(cur) - rank(prev));
+      }
+
       topics = topics.map(topic => ({
         id: topic.id,
         ...topic._doc,
         posts: topic.posts.length
       }));
-      
-      if ((req.params && req.params.sort === 'popular') || (!req.params || !req.params.sort)) {
-        // Sorting algorithm 1:
-        // (posts ^ 1.5) * (likes ^ .5)
-        // divided by
-        // ((lastPost.minutesAge + 120) ^ .75)
-        const rank = topic => (topic.posts ** 1.5) * (topic.likeCount ** 0.5) / (((moment().diff(moment(topic.lastDate), 'hours')) + 2) ** 1.25);
-        topics = topics.sort((prev, cur) => rank(cur) - rank(prev));
-      }
 
       res.status(200).send({
         authenticated: true,
