8c7d671fde32dc092d05eb7d12bfcbe29c8e6c0d
Buğra Gündüz <bgrgndzz@gmail.com>
2019-02-16 00:45:04 +0300

    add the unique property to mongoose where necessary

--- server/models/User/User.js
+++ server/models/User/User.js
@@ -10,7 +10,8 @@ const UserSchema = new Schema({
   auth: {
     email: {
       type: String,
-      required: true
+      required: true,
+      unique: true
     },
     password: {
       type: String,
